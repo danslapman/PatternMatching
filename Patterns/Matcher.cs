2474ea364c43958b00230263a80c8f58a70b37a0
Daniil Smirnov <danslapman@gmail.com>
2014-06-27 16:36:44 +0400

    Action-match implementation. ExprMatcher<> removed as deprecated.

--- Patterns/Matcher.cs
+++ Patterns/Matcher.cs
@@ -6,6 +6,94 @@ using System.Linq.Expressions;
 
 namespace Patterns
 {
+    public class Matcher<T> : IEnumerable<T>
+    {
+        private readonly List<BlockExpression> _caseExpressionsList = new List<BlockExpression>();
+
+        private ParameterExpression _parameter;
+        private ParameterExpression Parameter
+        {
+            get { return _parameter ?? (_parameter = Expression.Parameter(typeof(T), "inputValue")); }
+        }
+
+        private LabelTarget _retPoint;
+
+        private LabelTarget RetPoint
+        {
+            get { return _retPoint ?? (_retPoint = Expression.Label()); }
+        }
+
+        public void Add<TCtx>(Expression<Func<T, TCtx>> binder, Expression<Action<TCtx>> processor)
+        {
+            var bindResult = Expression.Variable(typeof(TCtx), "binded");
+            var caseExpr = Expression.Block(
+                new[] { bindResult },
+                Expression.Assign(bindResult, Expression.Invoke(binder, Parameter)),
+                Expression.IfThen(
+                    Expression.NotEqual(Expression.Convert(bindResult, typeof(object)), Expression.Constant(null)),
+                    Expression.Return(RetPoint, Expression.Invoke(processor, bindResult))
+                ));
+            _caseExpressionsList.Add(caseExpr);
+        }
+
+        public void Add(Expression<Predicate<T>> condition, Expression<Action<T>> processor)
+        {
+            var caseExpr = Expression.Block(
+                new Expression[]{
+                Expression.IfThen(
+                    Expression.Invoke(condition, Parameter),
+                    Expression.Return(RetPoint, Expression.Invoke(processor, Parameter))
+                    )});
+            _caseExpressionsList.Add(caseExpr);
+        }
+
+        private Action<T> CompileMatcher()
+        {
+            var finalExpressions = new Expression[]
+            {
+                Expression.Throw(Expression.Constant(new MatchException("Provided value was not matched with any case"))),
+                Expression.Label(RetPoint)
+            };
+
+            var matcherExpression = Expression.Block(_caseExpressionsList.Concat(finalExpressions)
+                );
+
+            return Expression.Lambda<Action<T>>(matcherExpression, Parameter).Compile();
+        }
+
+        private Action<T> _matcher;
+
+        private Action<T> MatcherFunc
+        {
+            get { return _matcher ?? (_matcher = CompileMatcher()); }
+        }
+
+        public void Match(T value)
+        {
+            MatcherFunc(value);
+        }
+
+        public static implicit operator Action<T>(Matcher<T> matcher)
+        {
+            return matcher.Match;
+        }
+
+        public Action<T> ToAction()
+        {
+            return Match;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException("This is a stub. There is nothing to enumerate in Matcher.");
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
     public class Matcher <TIn, TOut> : IEnumerable<TOut>
     {
         private readonly List<BlockExpression> _caseExpressionsList = new List<BlockExpression>();
