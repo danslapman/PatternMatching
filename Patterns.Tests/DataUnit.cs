b5aaf53e30f2306251deeee6c6affaa298d31832
Daniil Smirnov <danslapman@gmail.com>
2014-11-20 00:29:18 +0300

    Tests for Union equality methods

---
+++ Patterns.Tests/DataUnit.cs
@@ -0,0 +1,32 @@
+﻿namespace Patterns.Tests
+{
+    public class DataUnit
+    {
+        private readonly int _amount;
+
+        public int Amount { get { return _amount; } }
+
+        public DataUnit(int amount)
+        {
+            _amount = amount;
+        }
+
+        protected bool Equals(DataUnit other)
+        {
+            return _amount == other._amount;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Item) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _amount;
+        }
+    }
+}
---
+++ Patterns.Tests/UnionStructuralEqualityTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Patterns.Containers;
+using Xunit;
+
+namespace Patterns.Tests
+{
+    public class UnionStructuralEqualityTests
+    {
+        [Fact]
+        public void UnionEqualsItself()
+        {
+            var union = new Union<Item, DataUnit>(new Item(42));
+            Assert.True(union.Equals(union));
+        }
+
+        [Fact]
+        public void UnionEqualsSameUnion()
+        {
+            var union1 = new Union<Item, DataUnit>(new Item(42));
+            var union2 = new Union<Item, DataUnit>(new Item(42));
+            Assert.True(union1.Equals(union2));
+            Assert.True(union2.Equals(union1));
+        }
+
+        [Fact]
+        public void UnionIsNotEqualToStructurallyInequalUnion()
+        {
+            var union1 = new Union<Item, DataUnit>(new Item(42));
+            var union2 = new Union<Item, DataUnit>(new DataUnit(42));
+            Assert.False(union1.Equals(union2));
+            Assert.False(union2.Equals(union1));
+        }
+    }
+}
