1537deea29ea5ee49b41785b2d1dea5c1e7cfe80
Daniil Smirnov <danslapman@gmail.com>
2015-10-22 13:34:02 +0300

    Add Unit type

---
+++ Patterns/Unit.cs
@@ -0,0 +1,13 @@
+﻿namespace Patterns
+{
+    /// <summary>
+    /// Represents empty type
+    /// </summary>
+    public struct Unit
+    {
+        /// <summary>
+        /// Default instance of Unit
+        /// </summary>
+        public static Unit _ = new Unit();
+    }
+}
