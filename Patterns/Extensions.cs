8d1c1a3fba79da8fc43b19ecf7aba94c0ad96684
Daniil Smirnov <danslapman@gmail.com>
2015-10-26 14:53:53 +0300

    New helper extension methods

---
+++ Patterns/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using Patterns.Containers;
+
+namespace Patterns
+{
+    /// <summary>
+    /// Utility extensions
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Converts current value to Option&lt;T&gt;
+        /// </summary>
+        public static Option<T> ToOption<T>(this T instance)
+        {
+            return instance;
+        }
+
+        /// <summary>
+        /// Converts current value to Result&lt;TSucc, TErr&gt;
+        /// </summary>
+        /// <typeparam name="TSucc">Success type</typeparam>
+        /// <typeparam name="TErr">Failure type</typeparam>
+        public static Result<TSucc, TErr> ToResult<TSucc, TErr>(this TSucc instance)
+        {
+            return instance;
+        }
+
+        /// <summary>
+        /// Converts current value to Result&lt;TSucc, TErr&gt;
+        /// </summary>
+        /// <typeparam name="TSucc">Success type</typeparam>
+        /// <typeparam name="TErr">Failure type</typeparam>
+        public static Result<TSucc, TErr> ToResult<TSucc, TErr>(this TErr instance)
+        {
+            return instance;
+        }
+    }
+}
